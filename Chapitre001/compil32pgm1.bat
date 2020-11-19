8b3d5a8517a8bf634e3dc06ec85036645184ec3e
vincentARM <51370770+vincentARM@users.noreply.github.com>
2020-11-19 17:35:02 +0100

    Update compil32pgm1.bat

--- Chapitre001/compil32pgm1.bat
+++ Chapitre001/compil32pgm1.bat
@@ -2,7 +2,7 @@ Rem lancement compilation nasm
 Rem cd D:\Developpement\Windows\Assembleur32\Projets\projetTuto\Chapitre1
 cd .
 
-D:\Logiciels\NASM\nasm.exe -f win32  pgmCh1_1.asm
+D:\Logiciels\NASM\nasm.exe -f win32  pgmCh1_1.asm -l pgmCh1_1.txt
 
 D:\Logiciels\Golink\GoLink.exe pgmCh1_1.obj  /console Kernel32.dll User32.dll Gdi32.dll Comdlg32.dll  /entry:Main 
 pause

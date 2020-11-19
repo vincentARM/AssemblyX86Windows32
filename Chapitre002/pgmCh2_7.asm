2ea8b6429c02e057f081b996cacd4bda8e26336a
vincentARM <51370770+vincentARM@users.noreply.github.com>
2020-11-19 21:05:45 +0100

    Update pgmCh2_7.asm

--- Chapitre002/pgmCh2_7.asm
+++ Chapitre002/pgmCh2_7.asm
@@ -1,13 +1,13 @@
 ;programme pgmCh2_7
-; test acces à une zone mémoire
+; test acces Ã  une zone mÃ©moire
 
 ;=======================================
-; segment des données initialisées
+; segment des donnÃ©es initialisÃ©es
 ;=======================================
 segment .data 
 zone1:      dw 12
 ;=======================================
-; segment des données non initialisées
+; segment des donnÃ©es non initialisÃ©es
 ;=======================================
 segment .bss
 ;=======================================
@@ -17,8 +17,8 @@ segment .text
     global Main
     extern ExitProcess
 Main:
-    ;mov  eax,[zone1]
-    mov eax,zone1
+    mov  eax,[zone1]
+    ;mov eax,zone1
     push eax           ; met le code retour sur la pile
-    call ExitProcess   ; et retourne au système d'exploitation
-    ; le code retour peut être affiché dans PowerShell windows avec la commande echo $LASTEXITCODE
+    call ExitProcess   ; et retourne au systÃ¨me d'exploitation
+    ; le code retour peut Ãªtre affichÃ© dans PowerShell windows avec la commande echo $LASTEXITCODE

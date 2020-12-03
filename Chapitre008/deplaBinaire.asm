2bcb31e8e32850637420a54665038e243d97275f
vincentARM <51370770+vincentARM@users.noreply.github.com>
2020-12-03 18:29:37 +0100

    Add files via upload

---
+++ Chapitre008/deplaBinaire.asm
@@ -0,0 +1,360 @@
+;---programme deplaBinaire.asm 
+; conversion d'un registre en base 2
+; instructions de déplacement des bits
+
+STD_OUTPUT_HANDLE equ -11
+LONGUEUR equ 33
+;=======================================
+; segment des données initialisées
+;=======================================
+segment .data 
+szTitre        db "Win32", 0             ; titre de la fenêtre
+szMsgReg:      db 'Valeur du registre :', 0       ; message
+szRetourLigne: db 10,0
+
+szMessIndicateurs:  db "Zero : "
+bPos0:              db " "
+                    db " Signe: "
+bPosS:              db " "
+                    db " Carry: "
+bPosC:              db " "
+                    db " Offset: "
+bPosO:              db " "
+                    db 10,0
+;=======================================
+; segment des données non initialisées
+;=======================================
+segment .bss
+sZoneConv:         resb LONGUEUR
+;=======================================
+; segment de code
+;=======================================
+segment .text
+    global Main
+    extern MessageBoxA , ExitProcess, GetStdHandle, WriteFile
+Main:
+    push esp                 ; pour verifier que la pile est bien alignée en fin de programme
+    push sZoneConv
+    call conversion10S       ; conversion nombre signé
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+
+    mov eax,0b01100          ; deplacement gauche
+    shl eax,5
+    call verifierIndicateurs
+    push eax
+    push sZoneConv
+    call conversion2
+    push szMsgReg
+    call afficherConsole
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+    
+    mov eax,0b01100            ; deplacement droit
+    shl eax,2
+    push eax
+    push sZoneConv
+    call conversion2
+    push szMsgReg
+    call afficherConsole
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+    
+    mov eax,0b01001            ; 2 deplacements à droite
+    shr eax,1
+    call verifierIndicateurs   ; pour verifier le contenu du carry
+    shr eax,1
+    call verifierIndicateurs
+    push eax
+    push sZoneConv
+    call conversion2
+    push szMsgReg
+    call afficherConsole
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+    
+    mov eax,0b01100            ; deplacement gauche valeur contenue dans ecx
+    mov ecx,10
+    shl eax,cl                 ; il faut mettre la partie basse de ecx
+    push eax
+    push sZoneConv
+    call conversion2
+    push szMsgReg
+    call afficherConsole
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+    
+    mov eax,50            ; multiplication par 2 par deplacement à gauche
+    shl eax,1
+    push eax
+    push sZoneConv
+    call conversion10
+    push szMsgReg
+    call afficherConsole
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+    
+    mov eax,1000            ; division par 8 par deplacement à droite
+    shr eax,3
+    push eax
+    push sZoneConv
+    call conversion10
+    push szMsgReg
+    call afficherConsole
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+    
+    mov eax,0b110000000000000000000000000000 ; rotation à gauche
+    rol eax,4
+    push eax
+    push sZoneConv
+    call conversion2
+    push szMsgReg
+    call afficherConsole
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+    
+    mov eax,0b0110            ;  rotation à droite
+    ror eax,5
+    push eax
+    push sZoneConv
+    call conversion2
+    push szMsgReg
+    call afficherConsole
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+    
+    push esp                 ; pour verifier que la pile est bien alignée
+    push sZoneConv
+    call conversion10S       ; conversion nombre signé
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+    
+    push eax           ; met le code retour sur la pile
+    call ExitProcess   ; fin du programme 
+
+;**************************************
+;affichage console
+;**************************************
+afficherMessage:
+    push  ebp
+    mov ebp, esp
+    pusha                 ;sauvegarde des registres
+    pushf
+    mov edx, [ebp + 8]    ; recup de la valeur a afficher
+    push 0                ; uType = MB_OK
+    push dword szTitre    ; Titre de la fenÃªtre
+    push edx              ; message a afficher
+    push 0                ; hWnd = HWND_DESKTOP
+    call MessageBoxA
+    popf
+    popa                  ; restaur des registres
+    pop ebp
+    ret 4                 ;alignement pile car 1 push
+;**************************************
+;affichage console
+;**************************************
+afficherConsole:
+    push    ebp
+    mov ebp, esp
+    sub esp,8             ; reserve 8 octets pour le nombre de caractÃ¨res Ã©crits
+    pusha                 ;sauvegarde des registres
+    pushf
+    mov edx, [ebp + 8]    ; recup de la valeur a afficher
+    
+    mov ecx,0             ; compteur de caractÃ¨res
+.B1:                      ; boucle de calcul de la longueur
+    mov al,[edx,ecx]      ; charge un caractÃ¨re de la chaine
+    cmp al,0              ; si zÃ©ro c'est la fin de la chaine
+    je .B2
+    inc ecx               ; sinon incremente le compteur
+    jmp .B1               ; et boucle 
+.B2:
+    push    STD_OUTPUT_HANDLE
+    call    GetStdHandle  ; recherche du handle de la console
+    mov     ebx, eax      ; sauvÃ© dans ebx
+
+                          ; WriteFile( hstdOut, message, length(message), &bytes, 0);
+    push    0
+    mov    eax,ebp
+    sub     eax,8
+    push    eax            ; retour nb octets affiches ?
+    push    ecx            ; longueur du message
+    push    edx            ; adresse du message
+    push    ebx            ; handle de la console
+    call    WriteFile
+    popf
+    popa                   ; restaur des registres
+    add esp,8              ; libÃ¨re la place
+    pop ebp
+    ret 4                  ; alignement pile car 1 push
+;***************************************************
+;conversion en base 2 d'un registre en une chaine
+;**************************************************
+; parametre 1 le registre 
+; parametre 2 l'adresse de la zone receptrice
+TAILLE equ 32
+conversion2:
+    push    ebp
+    mov ebp, esp
+    pusha                  ;sauvegarde des registres
+    pushf
+    mov eax, [ebp + 12]    ; recup de la valeur a afficher
+    mov edi,[ebp + 8]      ; recup adresse zone de conversion
+    mov ecx,TAILLE-1
+    mov ebx ,2
+.A1:                       ; boucle de division par 16
+    mov edx,0
+    div ebx
+    add edx,'0'            ; ajout de '0' pour conversion ascii    
+    mov byte  [edi,ecx],dl ;et on place le caractere en position debut + 8
+    
+    dec ecx
+    cmp ecx,0              ;si pas taille atteinte on boucle
+    jge .A1
+    mov byte [edi+TAILLE],0 ;sinon on ajoute 0 en 32 ieme position pour terminer la chaine
+    popf                  ; fin routine
+    popa                  ; restaur des registres
+    pop ebp
+    ret 8                 ; alignement pile car 2 push
+;***************************************************
+;conversion en base 10 signée
+;avec suppression des zeros inutiles
+;****************************************************
+; parametre 1  valeur à convertir
+; parametre 2  zone de conversion longueur > 11
+conversion10S:
+    enter 0,0              ; prologue
+    pusha                  ;sauvegarde des registres
+    pushf
+    mov edi,[ebp+8]        ;recup adresse de la zone de conversion
+    mov BYTE [edi+LONGUEUR],0 ; stockage 0 final
+    mov eax, [ebp + 12]   ; recup de la valeur a afficher
+    cmp eax,0             ; compare à zéro
+    jl .A1                ; plus petit
+    mov dl,'+'            ; signe positif
+    jmp .A2
+.A1:
+    mov dl,'-'            ; signe négatif
+    neg eax               ; transforme en nombre positif
+.A2:    
+    mov byte [edi],dl     ; met le signe en position 0 de la zone
+    mov ecx,LONGUEUR-1
+    mov ebx ,10           ; diviseur
+.A3:
+    mov edx,0              ; division par 10
+    div ebx
+    add edx,'0'            ; conversion ascii du reste
+    mov byte  [edi,ecx],dl ; et mise en place dans zone de conversion
+    dec ecx
+    cmp eax,0              ;si division encore a faire
+    jne  .A3
+    inc ecx
+    mov eax,1
+.A4:                         ; recopie du résultat en début de zone de conversion
+    mov dl,[edi,ecx]
+    mov byte [edi,eax],dl
+    inc ecx
+    inc eax
+    cmp ecx,LONGUEUR         ; boucle jusqu'au 0 final
+    jle .A4
+                             ;fin
+    popf
+    popa                     ; restaur des registres
+    leave                    ; epilogue
+    ret  8
+;***************************************************
+;   Affichage etat indicateurs Z S C et O 
+;****************************************************
+
+verifierIndicateurs:
+    pusha                      ;sauvegarde des registres
+    pushf
+    jz .A1
+    mov BYTE [bPos0],'0'
+    jmp .A2
+.A1:
+    mov BYTE [bPos0],'1'
+.A2:
+    js .A3
+    mov BYTE [bPosS],'0'
+    jmp .A4
+.A3:
+    mov BYTE [bPosS],'1'
+.A4:
+    jc .A5
+    mov BYTE [bPosC],'0'
+    jmp .A6
+.A5:
+    mov BYTE [bPosC],'1'
+.A6:
+    jo .A7
+    mov BYTE [bPosO],'0'
+    jmp .A8
+.A7:
+    mov BYTE [bPosO],'1'
+.A8:
+    push szMessIndicateurs
+    call afficherConsole
+    popf
+    popa                       ; restaur des registres
+    ret 
+;***************************************************
+;conversion en base 10 non signée
+;avec suppression des zeros inutiles
+;et cadrage à gauche
+;****************************************************
+; parametre 1  valeur à convertir
+; parametre 2  zone de conversion longueur > 11
+conversion10:
+    enter 0,0
+    pusha                      ;sauvegarde des registres
+    pushf
+    mov edi,[ebp+8]            ;recup adresse de la zone de conversion
+    mov BYTE [edi+LONGUEUR],0  ; 0 final dans zone de conversion
+    mov eax, [ebp + 12]        ; recup de la valeur a afficher
+    mov ecx,LONGUEUR-1
+    mov ebx ,10
+.A1:                           ; début de boucle de calcul des restes successifs
+    mov edx,0                  ; division eax par 10
+    ;mov ebx ,10
+    div ebx
+    add edx,'0'                ; conversion ascii du reste
+    mov byte  [edi,ecx],dl
+    dec ecx
+    cmp eax,0                  ;si division encore a faire
+    jne  .A1
+    xor eax,eax                ; raz indice
+    inc ecx
+.A5:                           ; boucle de copie du résultat de ecx à LONGUEUR
+    mov dl,[edi,ecx]           ; charge un caractère du résultat
+    mov byte [edi,eax],dl      ; et le met au debut de la zone de conversion
+    inc ecx                    ; incremente le pointeur du résultat
+    inc eax                    ; incremente l'indice de reception
+    cmp ecx,LONGUEUR           ; boucle jusqu'au 0 final
+    jle .A5
+                               ; fin
+    popf
+    popa                       ; restaur des registres
+    leave
+    ret  8                     ;alignement pile car 2 push
---
+++ Chapitre008/operationsBinaire.asm
@@ -0,0 +1,305 @@
+;---programme operationsBinaire.asm 
+; conversion d'un registre en base 2
+; test de and or xor not et neg
+
+STD_OUTPUT_HANDLE equ -11
+LONGUEUR equ 33
+;=======================================
+; segment des données initialisées
+;=======================================
+segment .data 
+szTitre        db "Win32", 0             ; titre de la fenêtre
+szMsgReg:      db 'Valeur du registre :', 0       ; message
+szRetourLigne: db 10,0
+
+szMessIndicateurs:  db "Zero : "
+bPos0:              db " "
+                    db " Signe: "
+bPosS:              db " "
+                    db " Carry: "
+bPosC:              db " "
+                    db " Offset: "
+bPosO:              db " "
+                    db 10,0
+;=======================================
+; segment des données non initialisées
+;=======================================
+segment .bss
+sZoneConv:         resb LONGUEUR
+;=======================================
+; segment de code
+;=======================================
+segment .text
+    global Main
+    extern MessageBoxA , ExitProcess, GetStdHandle, WriteFile
+Main:
+    push esp                 ; pour verifier que la pile est bien alignée en fin de programme
+    push sZoneConv
+    call conversion10S       ; conversion nombre signé
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+
+    mov eax,0b01100          ; Et logique
+    mov ebx,0b01010
+    and eax,ebx
+    call verifierIndicateurs
+    push eax
+    push sZoneConv
+    call conversion2
+    push szMsgReg
+    call afficherConsole
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+    
+    mov eax,0b01100            ; ou logique
+    mov ebx,0b01010
+    or eax,ebx
+    push eax
+    push sZoneConv
+    call conversion2
+    push szMsgReg
+    call afficherConsole
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+    
+    mov eax,0b01100            ; ou exlusif
+    mov ebx,0b01010
+    xor eax,ebx
+    push eax
+    push sZoneConv
+    call conversion2
+    push szMsgReg
+    call afficherConsole
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+    
+    mov eax,0b01100            ; non logique
+    ;mov ebx,0b01010
+    not eax
+    push eax
+    push sZoneConv
+    call conversion2
+    push szMsgReg
+    call afficherConsole
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+    
+    mov eax,0b01100            ; negation
+    ;mov ebx,0b01010
+    neg eax
+    push eax
+    push sZoneConv
+    call conversion2
+    push szMsgReg
+    call afficherConsole
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+    
+    mov eax,12345            ; raz d'un registre
+    xor eax,eax
+    call verifierIndicateurs
+    push eax
+    push sZoneConv
+    call conversion2
+    push szMsgReg
+    call afficherConsole
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+    
+    mov eax,0b1100
+    test eax,0b1000          ; teste la valeur du bit 3
+    call verifierIndicateurs
+    test eax,0b0001          ; teste la valeur du bit 0
+    call verifierIndicateurs
+    
+    push esp                 ; pour verifier que la pile est bien alignée
+    push sZoneConv
+    call conversion10S       ; conversion nombre signé
+    push sZoneConv
+    call afficherConsole
+    push szRetourLigne
+    call afficherConsole
+    
+    push eax           ; met le code retour sur la pile
+    call ExitProcess   ; fin du programme 
+
+;**************************************
+;affichage console
+;**************************************
+afficherMessage:
+    push  ebp
+    mov ebp, esp
+    pusha                 ;sauvegarde des registres
+    pushf
+    mov edx, [ebp + 8]    ; recup de la valeur a afficher
+    push 0                ; uType = MB_OK
+    push dword szTitre    ; Titre de la fenÃªtre
+    push edx              ; message a afficher
+    push 0                ; hWnd = HWND_DESKTOP
+    call MessageBoxA
+    popf
+    popa                  ; restaur des registres
+    pop ebp
+    ret 4                 ;alignement pile car 1 push
+;**************************************
+;affichage console
+;**************************************
+afficherConsole:
+    push    ebp
+    mov ebp, esp
+    sub esp,8             ; reserve 8 octets pour le nombre de caractÃ¨res Ã©crits
+    pusha                 ;sauvegarde des registres
+    pushf
+    mov edx, [ebp + 8]    ; recup de la valeur a afficher
+    
+    mov ecx,0             ; compteur de caractÃ¨res
+.B1:                      ; boucle de calcul de la longueur
+    mov al,[edx,ecx]      ; charge un caractÃ¨re de la chaine
+    cmp al,0              ; si zÃ©ro c'est la fin de la chaine
+    je .B2
+    inc ecx               ; sinon incremente le compteur
+    jmp .B1               ; et boucle 
+.B2:
+    push    STD_OUTPUT_HANDLE
+    call    GetStdHandle  ; recherche du handle de la console
+    mov     ebx, eax      ; sauvÃ© dans ebx
+
+                          ; WriteFile( hstdOut, message, length(message), &bytes, 0);
+    push    0
+    mov    eax,ebp
+    sub     eax,8
+    push    eax            ; retour nb octets affiches ?
+    push    ecx            ; longueur du message
+    push    edx            ; adresse du message
+    push    ebx            ; handle de la console
+    call    WriteFile
+    popf
+    popa                   ; restaur des registres
+    add esp,8              ; libÃ¨re la place
+    pop ebp
+    ret 4                  ; alignement pile car 1 push
+;***************************************************
+;conversion en base 2 d'un registre en une chaine
+;**************************************************
+; parametre 1 le registre 
+; parametre 2 l'adresse de la zone receptrice
+TAILLE equ 32
+conversion2:
+    push    ebp
+    mov ebp, esp
+    pusha                  ;sauvegarde des registres
+    pushf
+    mov eax, [ebp + 12]    ; recup de la valeur a afficher
+    mov edi,[ebp + 8]      ; recup adresse zone de conversion
+    mov ecx,TAILLE-1
+    mov ebx ,2
+.A1:                       ; boucle de division par 16
+    mov edx,0
+    div ebx
+    add edx,'0'            ; ajout de '0' pour conversion ascii    
+    mov byte  [edi,ecx],dl ;et on place le caractere en position debut + 8
+    
+    dec ecx
+    cmp ecx,0              ;si pas taille atteinte on boucle
+    jge .A1
+    mov byte [edi+TAILLE],0 ;sinon on ajoute 0 en 32 ieme position pour terminer la chaine
+    popf                  ; fin routine
+    popa                  ; restaur des registres
+    pop ebp
+    ret 8                 ; alignement pile car 2 push
+;***************************************************
+;conversion en base 10 signée
+;avec suppression des zeros inutiles
+;****************************************************
+; parametre 1  valeur à convertir
+; parametre 2  zone de conversion longueur > 11
+conversion10S:
+    enter 0,0              ; prologue
+    pusha                  ;sauvegarde des registres
+    pushf
+    mov edi,[ebp+8]        ;recup adresse de la zone de conversion
+    mov BYTE [edi+LONGUEUR],0 ; stockage 0 final
+    mov eax, [ebp + 12]   ; recup de la valeur a afficher
+    cmp eax,0             ; compare à zéro
+    jl .A1                ; plus petit
+    mov dl,'+'            ; signe positif
+    jmp .A2
+.A1:
+    mov dl,'-'            ; signe négatif
+    neg eax               ; transforme en nombre positif
+.A2:    
+    mov byte [edi],dl     ; met le signe en position 0 de la zone
+    mov ecx,LONGUEUR-1
+    mov ebx ,10           ; diviseur
+.A3:
+    mov edx,0              ; division par 10
+    div ebx
+    add edx,'0'            ; conversion ascii du reste
+    mov byte  [edi,ecx],dl ; et mise en place dans zone de conversion
+    dec ecx
+    cmp eax,0              ;si division encore a faire
+    jne  .A3
+    inc ecx
+    mov eax,1
+.A4:                         ; recopie du résultat en début de zone de conversion
+    mov dl,[edi,ecx]
+    mov byte [edi,eax],dl
+    inc ecx
+    inc eax
+    cmp ecx,LONGUEUR         ; boucle jusqu'au 0 final
+    jle .A4
+                             ;fin
+    popf
+    popa                     ; restaur des registres
+    leave                    ; epilogue
+    ret  8
+;***************************************************
+;   Affichage etat indicateurs Z S C et O 
+;****************************************************
+
+verifierIndicateurs:
+    pusha                      ;sauvegarde des registres
+    pushf
+    jz .A1
+    mov BYTE [bPos0],'0'
+    jmp .A2
+.A1:
+    mov BYTE [bPos0],'1'
+.A2:
+    js .A3
+    mov BYTE [bPosS],'0'
+    jmp .A4
+.A3:
+    mov BYTE [bPosS],'1'
+.A4:
+    jc .A5
+    mov BYTE [bPosC],'0'
+    jmp .A6
+.A5:
+    mov BYTE [bPosC],'1'
+.A6:
+    jo .A7
+    mov BYTE [bPosO],'0'
+    jmp .A8
+.A7:
+    mov BYTE [bPosO],'1'
+.A8:
+    push szMessIndicateurs
+    call afficherConsole
+    popf
+    popa                       ; restaur des registres
+    ret 
